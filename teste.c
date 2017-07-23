7ace960fba730ba00494b205c4919c80cc9bffdf
NOTE\Alexandre <alexalsil11@gmail.com>
2017-07-23 11:36:34 -0300

    Teste de preenchimento de vetores

---
+++ teste.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vetor[7];
+
+        int i;
+
+        printf("Os elementos sao:\t");
+        for(i=0; i<7; i++)
+        {
+            printf("%2d ",vetor);
+        }
+        system("pause");
+        return 0;
+
+}
